f23ed19ea7b0318313877ce5674cdabcbce5d1a4
DI <cdi122@nate.com>
2022-07-24 16:25:54 +0900

    목록을 뿌려주는 담당인, Adapter 추가 작업 - ArrayAdapter를 물려받아서 기능활용 예정

---
+++ app/src/main/java/com/example/listviewpractice_dd/adapters/StudentAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.listviewpractice_dd.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.example.listviewpractice_dd.datas.Student
+
+
+class StudentAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+}
