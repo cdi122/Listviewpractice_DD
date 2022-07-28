8d86beab917ba2733f7a690d82af1d04a42af0ab
DI <cdi122@nate.com>
2022-07-28 17:18:04 +0900

    리스트뷰의 아이템 길게 누르는 이벤트 처리

--- app/src/main/java/com/example/listviewpractice_dd/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_dd/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         studentListview.setOnItemLongClickListener { parent, view, position, id ->
             val longClickedStudent = mStudentList[position]
             Toast.makeText(this, "${longClickedStudent.name} 이 길게 클릭됨", Toast.LENGTH_SHORT).show()
-            return@setOnItemLongClickListener true
+            return@setOnItemLongClickListener false
 
 
         }
