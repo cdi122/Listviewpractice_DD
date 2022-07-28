4aef18574774496d6ff3a3ac46aca0a284387a7e
DI <cdi122@nate.com>
2022-07-28 17:29:40 +0900

    리스트뷰의 아이템 길게 누르는 이벤트 처리

--- app/src/main/java/com/example/listviewpractice_dd/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_dd/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
             mAdapter.notifyDataSetChanged()
 
-            return@setOnItemLongClickListener false
+            return@setOnItemLongClickListener true
 
 
         }
